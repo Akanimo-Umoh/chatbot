46ede13164eda75f13c18f9d76a756e4de89e54a
Akanimo-Umoh <akanumoh4@gmail.com>
2025-10-29 10:17:01 +0100

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatbotUI from "./components/Chat";
+import Chat from "./components/Chat";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<ChatbotUI />} />
+          {/* <Route path="/" element={<Chat />} /> */}
         </Routes>
       </Router>
     </>
